182c054d7fc3e1566675bd1caf8e8a7038489b3e
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-07-22 09:58:10 +0300

    Add page header

--- src/index.js
+++ src/index.js
@@ -13,6 +13,19 @@ const renderContactPage = function() {
 }
 
 const rederWebiste = (function() {
-    
-
+    const header = document.createElement('div');
+    header.id = 'header';
+    const homeButton = document.createElement('button');
+    homeButton.id = 'home-button';
+    homeButton.textContent = 'Home';
+    header.appendChild(homeButton);
+    const menuButton = document.createElement('button');
+    menuButton.id = 'menu-button';
+    menuButton.textContent = 'Menu';
+    header.appendChild(menuButton);
+    const contactButton = document.createElement('button');
+    contactButton.id = 'contact-button';
+    contactButton.textContent = 'Contact';
+    header.appendChild(contactButton);
+    document.querySelector('#content').appendChild(header);
 })();
